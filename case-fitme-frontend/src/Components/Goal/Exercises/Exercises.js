92717424beba55059a553398a28b32f9c7760698
Veronica Andersen <97812447+VeronicaAndersen@users.noreply.github.com>
2022-09-08 15:45:36 +0200

    feat: printsout api for exercise & program

--- case-fitme-frontend/src/Components/Goal/Exercises/Exercises.js
+++ case-fitme-frontend/src/Components/Goal/Exercises/Exercises.js
@@ -1,37 +1,42 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Exercises = () => {
-    const [name, setName] = useState([]);
+
+    const [apiData, setApiData] = useState([]);
+    const api_url = `https://fitmecase.herokuapp.com/api/v1/exercise`
     useEffect(() => {
-        names()
+        fetch(api_url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setApiData(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
     }, []);
 
-    const names = async () => {
-        const api_url = `https://fitmecase.herokuapp.com/api/v1/exercise`;
-        const responce = await fetch(api_url)
-        setName(await responce.json());
-    }
     return (
         <>
             <div className="content">
                 <h1>Exercises</h1>
-                <table>
-                    {name.map((data) => {
-                        console.log(data);
-                        return (
-                            <div className="items">
-                                <div className="item">
-                                    <p>{data.name}</p>
-                                    <div>
-                                        <input type="number" min="1" placeholder="ex: 8" />
-                                        <button onClick={handleAddExer}>Add</button>
-                                    </div>
-                                </div>
+                <div className="items">
+                    {apiData.map(data =>
+                        <div className="item" key={data.id}>
+                            <p>{data.name} </p>
+                            <div>
+                                <input type="number" min="1" placeholder="ex: 8" />
+                                <button onClick={handleAddExer}>Add</button>
                             </div>
-                        )
-                    })}
-                </table>
-
+                        </div>
+                    )}
+                </div>
             </div>
         </>
     )
@@ -41,4 +46,3 @@ export default Exercises;
 const handleAddExer = () => {
     alert("Added");
 }
-
--- case-fitme-frontend/src/Components/Goal/Programs/Programs.js
+++ case-fitme-frontend/src/Components/Goal/Programs/Programs.js
@@ -1,6 +1,28 @@
 import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 
 const Programs = () => {
+
+    const [apiData, setApiData] = useState([]);
+    const api_url = `https://fitmecase.herokuapp.com/api/v1/program`
+    useEffect(() => {
+        fetch(api_url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setApiData(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
     let navigate = useNavigate();
     async function handleGoToWork(event) {
         event.preventDefault();
@@ -18,6 +40,13 @@ const Programs = () => {
                     <button onClick={handleGoToWork}>Go to Workouts</button>
                 </div>
             </div>
+            <div className="items">
+                    {apiData.map(data =>
+                        <div className="item" key={data.id}>
+                            <p>{data.name} </p>
+                        </div>
+                    )}
+                </div>
         </>
     )
 }
