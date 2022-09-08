27408b745a9ada1c7b629c5e7542c66586993f4e
Veronica Andersen <97812447+VeronicaAndersen@users.noreply.github.com>
2022-09-08 13:15:42 +0200

    feat: connect exercise to api.

--- case-fitme-frontend/src/Components/Goal/Exercises/Exercises.js
+++ case-fitme-frontend/src/Components/Goal/Exercises/Exercises.js
@@ -1,4 +1,23 @@
+import React, { useEffect } from 'react';
+
 const Exercises = () => {
+
+    useEffect(() => {
+        fetch(`https://fitmecase.herokuapp.com/api/v1/exercise`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is ${response.status}`
+                    );
+                }
+                return response.json();
+            })
+            .then((actualData) => console.log(actualData))
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
     return (
         <>
             <div className="content">
@@ -7,7 +26,7 @@ const Exercises = () => {
                     <div className="item">
                         <p>Item</p>
                         <div>
-                            <input type="number" min="1" placeholder="ex: 8"/>
+                            <input type="number" min="1" placeholder="ex: 8" />
                             <button onClick={handleAddExer}>Add</button>
                         </div>
                     </div>
