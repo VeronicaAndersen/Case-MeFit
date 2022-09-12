4600de2e416b65b6444441e42121fa82f6bb7f95
Veronica Andersen <97812447+VeronicaAndersen@users.noreply.github.com>
2022-09-12 07:33:17 +0200

    fix: updated profile settings

--- case-fitme-frontend/src/Components/Profiles/Profiles.js
+++ case-fitme-frontend/src/Components/Profiles/Profiles.js
@@ -8,8 +8,11 @@ const Profiles = () => {
                 <input type="password" className="input-form" placeholder=" Password" />
                 <input type="number" className="input-form" placeholder=" Weight" />
                 <input type="number" className="input-form" placeholder=" Height" />
+                <label for="img">Upload profile picture:</label>
+                <input type="file" id="img" name="img" accept="image/*"></input>
                 <button id="contri-btn">Request being contributor</button>
                 <button id="fa-btn">Add 2FA</button>
+                <button id="save-btn">Save</button>
             </div>
         </>
     )
