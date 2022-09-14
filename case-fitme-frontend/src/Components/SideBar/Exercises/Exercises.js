4d856d493961fbd0ce85a5731fa45d28a99e40a1
Veronica Andersen <97812447+VeronicaAndersen@users.noreply.github.com>
2022-09-14 13:52:04 +0200

    fix: form for exercise

--- case-fitme-frontend/src/Components/SideBar/Exercises/Exercises.js
+++ case-fitme-frontend/src/Components/SideBar/Exercises/Exercises.js
@@ -7,8 +7,11 @@ const apiUrl = process.env.REACT_APP_API_URL
 var counter = 0;
 const Exercises = () => {
     const [apiData, setApiData] = useState([]);
-    const { register, handleSubmit } = useForm()
-    const [apiError, setApiError] = useState(null)
+    const { register, handleSubmit } = useForm();
+    const [apiError, setApiError] = useState(null);
+
+
+
 
     useEffect(() => {
         fetch(`${apiUrl}/exercise`)
@@ -44,7 +47,26 @@ const Exercises = () => {
             <div className="content">
                 <h1>Exercises</h1>
                 <div className="items">
+                    <div className='item none'>
+                        <button onClick={handleAddExercise}>Create new exercise</button>
+                    </div>
 
+                    <form id='createExercise' onSubmit={handleSubmit(onSubmit)}>
+                        <h1>Create new Exercise</h1>
+                        <span className='close' onClick={handleClose}>X</span>
+                        <input className='input-form' type="text" placeholder='Name' {...register("name")} />
+                        <input className='input-form' type="text" placeholder='Description' {...register("description")} />
+                        <input className='input-form' type="text" placeholder='Target muscle group' {...register("targetMuscleGroup")} /> 
+                        <input className='input-form' type="text" placeholder='ImageURL' {...register("image")} />
+                        <input className='input-form' type="text" placeholder='VideoLink' {...register("videoLink")} />
+
+                        {/* <input type="checkbox" id="css" name="achieved" value="true" {...register("achieved")} />
+                            <label for="achieved">Achieved</label><br></br> */}
+                        <br/>
+                        <div className='item none'>
+                            {<button type="submit" value="Submit">Submit</button>}
+                        </div>
+                    </form>
                     {apiData.map((data) => {
                         if (data.id != null) {
 
@@ -52,9 +74,19 @@ const Exercises = () => {
                             return (
                                 <div className="item" key={data.id}>
                                     <p>{data.name} </p>
+                                    <div className='details' id={data.id}>
+                                        <p>{data.description}</p>
+                                        <p>{data.targetMuscleGroup}</p>
+                                        <img src={data.image} alt="img" />
+                                    </div>
                                     <div>
                                         <input className='input-number' type="number" min="1" placeholder="ex: 8" />
-                                        <button onClick={handleAddToWork}>Add</button>
+                                        <span>
+                                            <button value={data.id}>Details</button>
+                                            <button value={data.id}>Delete</button>
+                                            <button value={data.id}>Edit</button>
+                                            <button onClick={handleAddToWork}>Add</button>
+                                        </span>
                                     </div>
                                 </div>
                             )
@@ -69,13 +101,6 @@ const Exercises = () => {
                         }
                     })}
                 </div>
-
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input className='input-form' type="text" placeholder='Name' {...register("name")} />
-                    <input className='input-form' type="text" placeholder='Description' {...register("description")} />
-                    <input className='input-form' type="text" placeholder='Target muscle group' {...register("target_muscle_group")} /><br />
-                    {<input type="submit" value="Submit" />}
-                </form>
             </div>
         </>
     )
@@ -85,3 +110,10 @@ export default Exercises;
 const handleAddToWork = () => {
     alert("Added to workout");
 }
+const handleAddExercise = () => {
+    document.getElementById("createExercise").style.display = "block";
+}
+const handleClose = () => {
+    document.getElementById("createExercise").style.display = "none";
+}
+
