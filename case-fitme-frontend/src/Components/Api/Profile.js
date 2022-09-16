75fb28f27cbcb1a1abe46852c6d49de7f3b29cad
suay-selay <70511142+suay-selay@users.noreply.github.com>
2022-09-16 13:03:31 +0200

    feat: profile api added and profile component fixed

--- case-fitme-frontend/src/Components/Api/Profile.js
+++ case-fitme-frontend/src/Components/Api/Profile.js
@@ -3,16 +3,16 @@ import keycloak from '../../Keycloak/keycloak';
 
 const apiUrl = process.env.REACT_APP_API_URL
 
-//Create a new user (takes in array of program information)
-export const createProgram = async (programInfo) => {
+//Create a new user (takes in array of profile information)
+export const createProfile = async (profileInfo) => {
     try {
-        const response = await fetch(`${apiUrl}/program`, {
+        const response = await fetch(`${apiUrl}/profile`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${keycloak.token}`},
-            body: JSON.stringify(programInfo)
+            body: JSON.stringify(profileInfo)
         })
         if (!response.ok) {
-            throw new Error('Could not create new program')
+            throw new Error('Could not create new profile')
         }
         const data = await response.json()
         return [ null, data ]
@@ -22,13 +22,13 @@ export const createProgram = async (programInfo) => {
     }
 }
 
-//Update an existing program
-export const updateProgram = async (programInfo, programId) => {
+//Update an existing profile
+export const updateProfile = async (profileInfo, profileId) => {
     try {
-        if(programId === undefined){
-            throw new Error("Program ID is undefined");
+        if(profileId === undefined){
+            throw new Error("Profile ID is undefined");
         }
-        const response = await axios.put(`${apiUrl}/program/${programId}`, programInfo, {
+        const response = await axios.put(`${apiUrl}/profile/${profileId}`, profileInfo, {
             headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${keycloak.token}`}
         })
         return [ null, response.data ]
@@ -38,18 +38,18 @@ export const updateProgram = async (programInfo, programId) => {
     }
 }
 
-//Delete an existing program (takes in a program object)
-export const deleteProgram = async (programId) => {
+//Delete an existing profile (takes in a profile object)
+export const deleteProfile = async (profileId) => {
     try {
-        const response = await fetch(`${apiUrl}/program/${programId}`, {
+        const response = await fetch(`${apiUrl}/profile/${profileId}`, {
             method: 'DELETE',
             headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${keycloak.token}`},
             body: JSON.stringify({
-                programs: []
+                profiles: []
             })
         })
         if (!response.ok) {
-            throw new Error('Could not delete the program')
+            throw new Error('Could not delete the profile')
         }
         const result = await response.json()
         return [ null, result ]
@@ -58,20 +58,20 @@ export const deleteProgram = async (programId) => {
         return [ error.message, null ]
     }
 }
-//Update an existing program with workoutId
-export const uppdateProgram = async (programId, workout) => {
+//Update an existing profile with workoutId
+export const uppdateProfile = async (profileId, workout) => {
     try {
-        if(programId === undefined){
-            throw new Error("Program id is undefined");
+        if(profileId === undefined){
+            throw new Error("Profile id is undefined");
         }
         if(workout === undefined){
             throw new Error("Workout body is undefined");
         }
-        const response = await fetch(`${apiUrl}/program/${programId}`, {
+        const response = await fetch(`${apiUrl}/profile/${profileId}`, {
             method: 'PUT',
             headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${keycloak.token}`},
             body: JSON.stringify({
-                id: programId, name: workout.name, category: null, workouts: [workout.id], profiles: []
+                id: profileId, name: workout.name, category: null, workouts: [workout.id], profiles: []
             })
         })
         return [null, response.data]
