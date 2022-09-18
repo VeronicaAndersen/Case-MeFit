2093504fe91bee570d6ff1f69cbb69dcd8d81543
Veronica Andersen <97812447+VeronicaAndersen@users.noreply.github.com>
2022-09-18 16:31:25 +0200

    fix: exercise add set

--- case-fitme-frontend/src/Components/Api/Exercise.js
+++ case-fitme-frontend/src/Components/Api/Exercise.js
@@ -71,27 +71,27 @@ export const deleteExercise = async (exerciseId) => {
 }
 
 //Update an existing program with workoutId
-export const updateExerciseSet = async (programInfo, programId, workout) => {
+export const updateExerciseSet = async (exerciseInfo, exerciseId, set) => {
     try {
-        if (programId === undefined) {
-            throw new Error("Program id is undefined");
+        if (exerciseId === undefined) {
+            throw new Error("Exercise id is undefined");
         }
-        if (workout === undefined) {
-            throw new Error("Workout body is undefined");
+        if (set === undefined) {
+            throw new Error("Set body is undefined");
         }
-        let updatedProgram = null;
-        for (let i = 0; i < programInfo.length; i++) {
-            if (programInfo[i].id == programId) {
-                updatedProgram = programInfo[i];
+        let updatedExercise = null;
+        for (let i = 0; i < exerciseInfo.length; i++) {
+            if (exerciseInfo[i].id === exerciseId) {
+                updatedExercise = exerciseInfo[i];
                 break;
             }
         }
-        updatedProgram.workouts = [workout]
-        console.log(updatedProgram);
-        const response = await fetch(`${apiUrl}/program/${programId}`, {
+        updatedExercise.sets = [set]
+        console.log(updatedExercise);
+        const response = await fetch(`${apiUrl}/exercise/${exerciseId}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${keycloak.token}` },
-            body: JSON.stringify(updatedProgram)
+            body: JSON.stringify(updatedExercise)
         })
         return [null, response.data]
     }
--- case-fitme-frontend/src/Components/ArchivedGoal/ArchivedGoals.js
+++ case-fitme-frontend/src/Components/ArchivedGoal/ArchivedGoals.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 const apiUrl = process.env.REACT_APP_API_URL
-var counter = 0;
+
 const ArchivedGoals = () => {
 
     const [apiData, setApiData] = useState([]);
@@ -27,29 +27,19 @@ const ArchivedGoals = () => {
             <h1>Archived Goals</h1>
 
             {apiData.map((data) => {
-                if (data.complete === true) {
-                    counter++;
-                    return (
-                        <div key={data.id} className="weekly-schedule">
-                            <div className="weekly-todo">
-                                <p className="workout">{data.name}</p>
-                                {<div>
-                                    <p className="type">{data.type}</p>
+                return (
+                    <div key={data.id} className="weekly-schedule">
+                        <div className="weekly-todo">
+                            <p className="workout">{data.name}</p>
+                            {<div>
+                                <p className="type">{data.type}</p>
 
-                                </div>}
-                                <div className="circle" id='item-complete'></div>
-                            </div>
-                        </div>)
-                }else if (counter === 0) {
-                    return (
-                        <div className="weekly-schedule" key="0">
-                            <div className="weekly-todo">
-                                <p>No archived yet!</p>
-                            </div>
+                            </div>}
+                            <div className="circle" id='item-complete'></div>
                         </div>
-                    )
-                }
-            })}
+                    </div>)
+            }
+            )}
         </>
     )
 }
