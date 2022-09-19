e4329cb25a266c3cd6ec71fe4266157bc54ed1c3
Adrian Mattsson <adrian.mattsson@se.experis.com>
2022-09-19 09:46:55 +0200

    feat: added goals that show up in goal lists

--- case-fitme-frontend/src/Components/ArchivedGoal/ArchivedGoals.js
+++ case-fitme-frontend/src/Components/ArchivedGoal/ArchivedGoals.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import keycloak from '../../Keycloak/keycloak';
+
 const apiUrl = process.env.REACT_APP_API_URL
 var counter = 0;
 const ArchivedGoals = () => {
 
     const [apiData, setApiData] = useState([]);
     useEffect(() => {
-        fetch(`${apiUrl}/workout`)
+        fetch(`${apiUrl}/goal`, {
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${keycloak.token}` },
+        })
             .then((response) => {
                 if (!response.ok) {
                     throw new Error(
@@ -20,34 +24,35 @@ const ArchivedGoals = () => {
             .catch((err) => {
                 console.log(err.message);
             });
-    }, [])
+    }, []);
 
     return (
         <>
-            <h1>Archived Goals</h1>
-
-            {apiData.map((data) => {
+            {apiData.map(data => {
                 if (data.complete === true) {
-                    counter++;
                     return (
                         <div key={data.id} className="weekly-schedule">
                             <div className="weekly-todo">
-                                <p className="workout">{data.name}</p>
+                                <p className="workout">{data.goalName}</p>
                                 {<div>
-                                    <p className="type">{data.type}</p>
+                                    <p className="type">{data.date}</p>
 
                                 </div>}
                                 <div className="circle" id='item-complete'></div>
                             </div>
                         </div>)
-                }else if (counter === 0) {
+                } else {
                     return (
-                        <div className="weekly-schedule" key="0">
+                        <div key={data.id} className="weekly-schedule">
                             <div className="weekly-todo">
-                                <p>No archived yet!</p>
+                                <p className="workout">{data.goalName}</p>
+                                {<div>
+                                    <p className="type">{data.date}</p>
+
+                                </div>}
+                                <div className="circle"></div>
                             </div>
-                        </div>
-                    )
+                        </div>)
                 }
             })}
         </>
